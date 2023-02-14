b54a45501a063041f460252a28d5f991dff5178a
BartlomiejF <30440380+BartlomiejF@users.noreply.github.com>
2023-02-14 22:16:06 +0100

    Delete webapp directory

--- webapp/forms.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
-
-class ChromophoreForm(FlaskForm):
-    chromophore = StringField("Chromophore SMILES", validators=[DataRequired()])
-    # solvent = StringField("Solvent SMILES")
-    submit = SubmitField('Predict')
